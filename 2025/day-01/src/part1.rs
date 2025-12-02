4728e31d94d70682fe20f614072f2ee48046a8e8
mcbernie <nb@zweiund40.com>
2025-12-02 21:35:45 +0100

    day-01

--- 2025/day-01/src/part1.rs
+++ 2025/day-01/src/part1.rs
@@ -1,6 +1,16 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+
+    input.lines().
+
+}
+
+/// which direction to turn how many steps
+/// starts by 50. after 0 we wrap around to 99
+/// at the end we count the number of time we cross 0
+enum Direction {
+    Left(i32),
+    Right(i32),
 }
 
 #[cfg(test)]
@@ -10,7 +20,17 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         todo!("haven't built test yet");
-        let input = "";
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+        ";
         assert_eq!("", process(input)?);
         Ok(())
     }
